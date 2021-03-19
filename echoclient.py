d00e86ceb86ef57f56cec48d11256c27bab4a928
JosunLP <jolun.swtor@gmail.com>
2021-03-19 01:34:57 +0100

    adding line cleanup

--- echoclient.py
+++ echoclient.py
@@ -5,9 +5,12 @@ from datetime import datetime
 from helper import MessageParser
 from helper import const
 import threading
+import sys
 
 HOST = const.HOST
 PORT = const.PORT
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
 
 
 def message_listener(s: socket):
@@ -28,6 +31,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     threading.Thread(target=message_listener, args=(s))
     while True:
         string = input()
+        sys.stdout.write(CURSOR_UP_ONE)
+        sys.stdout.write(ERASE_LINE)
         if "!logout" in string:
             break
         m = Message.Message(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), string, UNAME)
