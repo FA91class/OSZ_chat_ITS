f2198b29f0d472e47f90c79f5a1b0ac625bcca15
JosunLP <jolun.swtor@gmail.com>
2021-03-19 11:21:26 +0100

    fix

--- model/Session.py
+++ model/Session.py
@@ -1,4 +1,5 @@
 from model import Message
+from helper import MessageParser
 import socket
 
 
@@ -7,7 +8,8 @@ class Session:
     def __init__(self):
         self.socketList = []
 
-    def broadcast_message(self, msg: Message.Message, s: socket.socket):
+    def broadcast_message(self, msg: bytearray, s: socket.socket):
         for cSocket in self.socketList:
             if cSocket != s:
                 cSocket.sendall(msg)
+                log: Message.Message = MessageParser.byte_array_to_message(msg)
